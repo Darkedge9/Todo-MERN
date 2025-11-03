9e1ea3fb9fc8f9d88f25df7b77610c77cc2b27a2
Shubham Bharti <shubhambharti409sb5019175@gmail.com>
2025-11-03 19:47:53 +0530

    change some file directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import home from "./components/home.jsx"
-import Login from "./components/Login.jsx"
-import Signup from "./components/Signup.jsx"
-import { Routes, Route } from 'react-router-dom'
-import PageNotFound from './components/PageNotFound.jsx'
-import { Toaster } from 'react-hot-toast';
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
+import { Routes, Route } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound.jsx";
+import { Toaster } from "react-hot-toast";
+import { Navigate } from "react-router-dom";
 
 function App() {
-
-  const token = localStorage.getItem("jwt")
+  const token = localStorage.getItem("jwt");
   return (
-    <div >
+    <div>
       <Routes>
-        <Route path="/" element ={token?<home/> : <Navigate to  ={"/login"} />} />
-        <Route path="/Login" element ={<Login/>} />
-        <Route path="/signup" element ={<Signup/>} />
-        <Route path="*" element ={<PageNotFound/>} />
+        <Route
+          path="/"
+          element={token ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Toaster />
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
